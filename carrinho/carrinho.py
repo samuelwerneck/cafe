a129a1458efc82078132066f00ca331ec4cd4fd8
Samuel Werneck <werneck.mg@gmail.com>
2024-10-16 19:50:32 -0300

    Implementando funções do carrinho: adicionar, atualizar e remover itens

--- carrinho/carrinho.py
+++ carrinho/carrinho.py
@@ -1,3 +1,5 @@
+from store.models import Produto
+
 
 class Carrinho():
 	def __init__(self, request):
@@ -13,3 +15,51 @@ class Carrinho():
 
 		# Garante que o carrinho esta disponivel em todas as paginas do site
 		self.carrinho = carrinho
+	
+	def add(self, produto, quantidade):
+		produto_id = str(produto.id)
+		produto_qtde = str(quantidade)
+
+		if produto_id in self.carrinho:
+			pass
+		else:
+			#self.carrinho[produto_id] = {'Preço :': str(produto.preco)}
+			self.carrinho[produto_id] = int(produto_qtde)
+
+		self.session.modified = True
+
+	def __len__(self):
+		return len(self.carrinho)
+	
+	def get_produtos(self):
+		# Obtem os IDs do carrinho
+		produto_ids = self.carrinho.keys()
+		# Usa os ids pra buscar os produtos no banco
+		produtos = Produto.objects.filter(id__in=produto_ids)
+		
+
+		return produtos
+	
+	def get_quantidades(self):
+		quantidades = self.carrinho
+		return quantidades
+	
+	def atualizar(self, produto, quantidade):
+		produto_id = str(produto)
+		produto_quantidade = int(quantidade)
+
+		meucarrinho = self.carrinho
+		meucarrinho[produto_id] = produto_quantidade
+
+		self.session.modified = True
+
+		mudanca = self.carrinho
+		return mudanca
+	
+	def delete (self, produto):
+		produto_id = str(produto)
+		# Deleta do dicionario/carrinho
+		if produto_id in self.carrinho:
+			del self.carrinho[produto_id]
+		
+		self.session.modified = True
--- carrinho/views.py
+++ carrinho/views.py
@@ -1,13 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .carrinho import Carrinho
+from store.models import Produto
+from django.http import JsonResponse
 
 def carrinho_resumo(request):
-	return render(request, "carrinho_resumo.html", {})
+	# Obtem o carrinho
+	carrinho = Carrinho(request)
+	carrinho_produtos = carrinho.get_produtos
+	quantidades = carrinho.get_quantidades
+	return render(request, "carrinho_resumo.html", {"carrinho_produtos":carrinho_produtos, "quantidades":quantidades})
 
 def carrinho_add(request):
-	pass
+	# Obtem o Carrinho
+	carrinho = Carrinho(request)
+	# Teste do POST
+	if request.POST.get('action') == 'post':
+		# Obtem os produtos no carrinho
+		produto_id = int(request.POST.get('produto_id'))
+		produto_quantidade = int(request.POST.get('produto_qtde'))
+
+
+		produto = get_object_or_404(Produto, id=produto_id)
+		
+		# Salva para a sessao
+		carrinho.add(produto=produto, quantidade=produto_quantidade)
+		
+		# Obtem a quantidade de itens no carrinho
+		carrinho_quantidade = carrinho.__len__()
+
+		# Retorna a resposta
+		# resposta = JsonResponse({'Nome do Produto: ': produto.nome})
+		resposta = JsonResponse({'qtde': carrinho_quantidade})
+		return resposta
+	
 
 def carrinho_delete(request):
-	pass
+	carrinho = Carrinho(request)
+	if request.POST.get('action') == 'post':
+		# Obtem os produtos no carrinho
+		produto_id = int(request.POST.get('produto_id'))
+		# Chama a função deletar no carrinho
+		carrinho.delete(produto=produto_id)
+
+		resposta = JsonResponse({'produto':produto_id})
+		return resposta
+
+
 
 def carrinho_update(request):
-	pass
+	carrinho = Carrinho(request)
+	if request.POST.get('action') == 'post':
+		# Obtem os produtos no carrinho
+		produto_id = int(request.POST.get('produto_id'))
+		produto_quantidade = int(request.POST.get('produto_qtde'))
+
+		carrinho.atualizar(produto=produto_id, quantidade=produto_quantidade)
+
+		resposta = JsonResponse({'qtde':produto_quantidade})
+		return resposta
