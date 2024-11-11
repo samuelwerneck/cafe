269efb5a9eeeb51c12c0e124aecb466826c7bf97
Samuel Werneck <werneck.mg@gmail.com>
2024-11-11 20:59:36 -0300

    Ajustes de nagegação e página de admin

--- cafe/settings.py
+++ cafe/settings.py
@@ -121,8 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = ['static/']
+STATIC_URL = '/staticfiles/'
+STATICFILES_DIRS = ['/staticfiles/']
 
 # Conteúdo estático
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
